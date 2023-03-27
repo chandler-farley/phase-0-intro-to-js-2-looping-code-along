eae930fa703a65757e7a93d09478f5f07f59f8ba
Chandler Farley <chandlerfarley@live.com>
2023-03-27 16:24:07 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+
+/* for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+} */
+
+/* const gifts = ["teddy bear", "drone", "doll"];
+function wrapGrifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+    return gifts;
+}
+wrapGrifts(gifts); */
+
+const tyCards = [];
+function writeCards(cards, occasion) {
+    for (let i = 0; i < cards.length; i++) {
+        tyCards.push(`Thank you, ${cards[i]}, for the wonderful ${occasion} gift!`);
+    };
+    return tyCards;
+}
+
+function countDown(x) {
+    while (x >= 0) {
+        console.log(x--);
+    };
+}
